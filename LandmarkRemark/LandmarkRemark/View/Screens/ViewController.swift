a13e05d70aa00ac01bfe93fb6bede5101a13282f
arvinq <arvin.quiliza@gmail.com>
2021-09-20 00:32:33 +1000

    Programmatic and File group setup

---
+++ LandmarkRemark/LandmarkRemark/View/Screens/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LandmarkRemark
+//
+//  Created by Arvin Quiliza on 9/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
