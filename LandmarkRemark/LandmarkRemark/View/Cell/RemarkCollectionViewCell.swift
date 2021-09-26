02ae8c9c281239c9298d7a55939a26b44d0eed9b
arvinq <arvin.quiliza@gmail.com>
2021-09-27 02:33:56 +1000

    Added both latitude and longitude in cell

--- LandmarkRemark/LandmarkRemark/View/Cell/RemarkCollectionViewCell.swift
+++ LandmarkRemark/LandmarkRemark/View/Cell/RemarkCollectionViewCell.swift
@@ -14,6 +14,9 @@ class RemarkCollectionViewCell: UICollectionViewCell {
     
     var noteTitleLabel = UILabel()
     var separatorView = LRSeparatorView()
+    var textStackview = UIStackView()
+    var latitudeLabel = UILabel()
+    var longitudeLabel = UILabel()
     
     // will be useful when we need to bind multiple values from our viewModel to our cell
     var remarkViewModel: RemarkViewModel? {
@@ -37,11 +40,25 @@ class RemarkCollectionViewCell: UICollectionViewCell {
     }
     
     private func setupView() {
-        
         noteTitleLabel.font = UIFont.systemFont(ofSize: 18.0, weight: .bold)
         noteTitleLabel.textColor = .label
         noteTitleLabel.translatesAutoresizingMaskIntoConstraints = false
         addSubview(noteTitleLabel)
+
+        textStackview.axis = .horizontal
+        textStackview.alignment = .fill
+        textStackview.distribution = .fillProportionally
+        textStackview.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(textStackview)
+        
+        // latitude and longitude labels
+        latitudeLabel.font = UIFont.systemFont(ofSize: 12.0, weight: .medium)
+        latitudeLabel.textColor = .secondaryLabel
+        textStackview.addArrangedSubview(latitudeLabel)
+        
+        longitudeLabel.font = UIFont.systemFont(ofSize: 12.0, weight: .medium)
+        longitudeLabel.textColor = .secondaryLabel
+        textStackview.addArrangedSubview(longitudeLabel)
         
         addSubview(separatorView)
     }
@@ -50,7 +67,12 @@ class RemarkCollectionViewCell: UICollectionViewCell {
         NSLayoutConstraint.activate([
             noteTitleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Space.padding),
             noteTitleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: Space.padding),
-            noteTitleLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            noteTitleLabel.bottomAnchor.constraint(equalTo: textStackview.topAnchor, constant: -Space.padding),
+            
+            //latitude and longitude
+            textStackview.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Space.padding),
+            textStackview.trailingAnchor.constraint(equalTo: trailingAnchor, constant: Space.padding),
+            textStackview.bottomAnchor.constraint(equalTo: separatorView.topAnchor, constant: -Space.padding),
             
             separatorView.heightAnchor.constraint(equalToConstant: Size.separatorHeight),
             separatorView.widthAnchor.constraint(equalTo: widthAnchor),
@@ -62,5 +84,7 @@ class RemarkCollectionViewCell: UICollectionViewCell {
     
     private func bindValues() {
         noteTitleLabel.text = remarkViewModel?.title
+        latitudeLabel.text = "Latitude: \(remarkViewModel?.latitude ?? 0.0)"
+        longitudeLabel.text = "Latitude: \(remarkViewModel?.longitude ?? 0.0)"
     }
 }
